c07749abe5df554dd270d2bcf739427bc5a81810
zey <zeynepuyanmis@gmail.com>
2024-07-26 19:58:18 +0300

    Limitli paylaşım servisi

---
+++ Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using NDAccountManager.Data;
+
+namespace NDAccountManager.Services
+{
+    public class AccountService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SetIsSharedToFalse(int accountId)
+        {
+            var account = await _context.Accounts.FindAsync(accountId);
+            if (account != null)
+            {
+                account.IsShared = false;
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
